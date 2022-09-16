7fd561924d292db509aec7301f781bd356b29c66
hina-digitalflake <hina@digitalflake.com>
2022-09-16 16:35:23 +0530

    Initial Commit

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Hina Chauhan on 16/09/22.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
